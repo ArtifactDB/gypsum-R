fa71013d32a760a8ecac1e08deab3e0931a9dd1f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-02-23 17:46:24 -0800

    Add wildcard support, documentation.

--- R/searchMetadataText.R
+++ R/searchMetadataText.R
@@ -1,21 +1,16 @@
 #' Search a metadata database
 #'
 #' Perform a string search on a SQLite database containing metadata from the gypsum backend
-#' (see \url{https://github.com/ArtifactDB/bioconductor-metadata-index}).
+#' (see \url{https://github.com/ArtifactDB/bioconductor-metadata-index} for the table structure).
 #'
 #' @param query List or character vector specifying the query to execute, see Details.
+#' @param path String containing a path to a SQLite file, usually obtained via \code{\link{fetchMetadataDatabase}}.
 #' @param latest Logical scalar indicating whether to only search for matches within the latest version of each asset.
-#' Ignored if \code{where.only=TRUE}.
-#' @param where.only Logical scalar indicating whether to return the WHERE filter condition.
-#' Intended to help developers in assembling their own SQL queries involving a token search.
 #' @param include.metadata Logical scalar indicating whether metadata should be returned.
-#' Ignored if \code{where.only=TRUE}.
-#' @param pid.name String containing the name of the column of the \code{paths} table that contains the path ID.
-#' Only used if \code{where.only=TRUE}.
-#' @param name,cache,overwrite Further arguments to pass to \code{\link{fetchMetadataDatabase}}.
+#' @param pid.name String containing the name/alias of the column of the \code{paths} table that contains the path ID.
 #'
 #' @return 
-#' If \code{where.only=FALSE}, a data frame specifying the contaning the results of the query.
+#' For \code{searchMetadataText}, a data frame specifying the contaning the search results.
 #' \itemize{
 #' \item The \code{project}, \code{asset} and \code{version} columns contain the identity of the version with matching metadata.
 #' \item If \code{include.metadata=TRUE}, a \code{metadata} column is present with the nested metadata for each match.
@@ -23,17 +18,47 @@
 #' Otherwise, only the latest version is returned.
 #' }
 #'
-#' If \code{where.only=TRUE}, a list containing \code{where}, a string can be directly used as a WHERE filter condition;
+#' For \code{searchMetadataTextFilter}, a list containing \code{where}, a string can be directly used as a WHERE filter condition in a SQL SELECT statement;
 #' and \code{parameters}, the parameter bindings to be used in \code{where}.
 #' The return value may also be \code{NULL} if the query has no well-defined filter.
 #'
 #' @author Aaron Lun
 #'
+#' @details
+#' When \code{query} is a list, the SQL filter expression is constructed using the following rules:
+#' \itemize{
+#' \item If \code{query} has a \code{type} attribute set to \code{"and"}, it represents a boolean AND operation.
+#' Each item of \code{query} should be a nested list representing a subquery; all subqueries must match.
+#' \item If \code{query} has a \code{type} attribute set to \code{"or"}, it represents a boolean OR operation.
+#' Each item of \code{query} should be a nested list representing a subquery; at least one subquery must match.
+#' \item If \code{query} does not have a \code{type} attribute, it represents a query on a text string.
+#' \code{query} itself should contain a single character vector of length 1, representing the string to be matched.
+#' \itemize{
+#' \item If \code{query} has a \code{partial} attribute set to \code{TRUE}, the string is directly used for a wildcard match to the token in the database.
+#' This expects SQLite's \code{\%} and \code{_} wildcards.
+#' \item Otherwise, the string is tokenized and used for equality comparison to tokens in the database.
+#' If tokenization yields multiple tokens from the string, all tokens must have matches in the database for the text to be considered as matched.
+#' \item Additionally, \code{query} may have a \code{field} attribute, restricting the token matches to a particular metadata field.
+#' If this is not present, the search for matching tokens is performed across all available metadata fields.
+#' }
+#' }
+#'
+#' Each string is tokenized by converting it to lower case and splitting it on characters that are not Unicode letters/numbers or a dash.
+#' We currently do not remove diacritics so these will need to be converted to ASCII by the user. 
+#' If a text query involves only non-letter/number/dash characters, the filter will not be well-defined and will be ignored when constructing SQL statements.
+#'
+#' For convenience, any list inside \code{query} (or indeed, \code{query} itself) may be replaced by a non-empty character vector.
+#' A character vector of length 1 is treated as shorthand for a text query with no \code{partial} or \code{field} attributes.
+#' A character vector of length greater than 1 is treated as shorthand for an AND operation on text queries for each of the individual strings.
+#' 
 #' @seealso
 #' \code{\link{fetchMetadataDatabase}}, to download and cache the database files.
+#'
+#' \url{https://github.com/ArtifactDB/bioconductor-metadata-index}, for details on the SQLite file contents.
 #' 
 #' @examples
-#' searchMetadataText(c("mouse", "brain"), include.metadata=FALSE)
+#' path <- fetchMetadataDatabase()
+#' searchMetadataText(path, c("mouse", "brain"), include.metadata=FALSE)
 #'
 #' # Now for a slightly more complex query:
 #' tissue.query <- list("brain", "pancreas")
@@ -42,30 +67,22 @@
 #' attr(species.query, "field") <- "taxonomy_id"
 #' query <- list(tissue.query, species.query)
 #' attr(query, "type") <- "and"
-#' searchMetadataText(query, include.metadata=FALSE)
+#' searchMetadataText(path, query, include.metadata=FALSE)
+#'
+#' # Throwing in some wildcards.
+#' query <- list("neuro%")
+#' attr(query, "partial") <- TRUE
+#' searchMetadataText(path, query, include.metadata=FALSE)
 #' @export
-searchMetadataText <- function(query, latest=TRUE, include.metadata=TRUE, where.only=FALSE, pid.name="paths.pid", name="bioconductor.sqlite3", cache=cacheDirectory(), overwrite=FALSE) {
-    if (!where.only) {
-        pid.name <- "paths.pid"
+searchMetadataText <- function(path, query, latest=TRUE, include.metadata=TRUE) {
+    where <- searchMetadataTextFilter(query)
+    cond <- where$where
+    if (is.null(where)) {
+        params <- list()
+    } else {
+        params <- where$parameters
     }
 
-    query <- sanitize_query(query)
-    cond <- NULL
-    env <- new.env()
-    if (!is.null(query)) {
-        env$parameters <- list()
-        cond <- build_query(query, pid.name, env)
-    }
-
-    if (where.only) {
-        if (is.null(cond)) {
-            return(NULL)
-        } else {
-            return(list(where=cond, parameters=env$parameters))
-        }
-    }
-
-    path <- fetchMetadataDatabase(name, cache=cache, overwrite=overwrite)
     conn <- DBI::dbConnect(RSQLite::SQLite(), path)
     on.exit(DBI::dbDisconnect(conn))
 
@@ -77,6 +94,7 @@ searchMetadataText <- function(query, latest=TRUE, include.metadata=TRUE, where.
         stmt <- paste0(stmt, ", versions.latest AS latest")
     }
     stmt <- paste0(stmt, " FROM paths LEFT JOIN versions ON paths.vid = versions.vid")
+
     if (latest) {
         cond <- c(cond, "versions.latest = 1")
     }
@@ -84,11 +102,25 @@ searchMetadataText <- function(query, latest=TRUE, include.metadata=TRUE, where.
         stmt <- paste0(stmt, " WHERE ", paste(cond, collapse=" AND "))
     }
 
-    everything <- DBI::dbGetQuery(conn, stmt, params=env$parameters)
+    everything <- DBI::dbGetQuery(conn, stmt, params=params)
     if (include.metadata) {
         everything$metadata <- lapply(everything$metadata, fromJSON, simplifyVector=FALSE)
     }
     everything
+}
+
+#' @export
+#' @rdname searchMetadataText
+searchMetadataTextFilter <- function(query, pid.name = 'paths.pid') {
+    query <- sanitize_query(query)
+    if (is.null(query)) {
+        return(NULL)
+    }
+
+    env <- new.env()
+    env$parameters <- list()
+    cond <- build_query(query, pid.name, env)
+    list(where=cond, parameters=env$parameters)
 }
 
 sanitize_query <- function(query) {
@@ -120,7 +152,10 @@ sanitize_query <- function(query) {
     }
 
     if (!is.null(qt)) {
-        stop("'type' must be one of 'and', 'or' or NULL")
+        stop("'type' must be one of 'and', 'or' or absent")
+    }
+    if (isTRUE(attr(query, "partial"))) {
+        return(query)
     }
 
     text <- strsplit(tolower(query[[1]]), "[^\\p{N}\\p{L}\\p{Co}-]", perl=TRUE)[[1]]
@@ -154,12 +189,18 @@ build_query <- function(query, name, env) {
     qt <- attr(query, "type")
     if (is.null(qt)) {
         nt <- add_query_parameter(env, query[[1]])
+        if (isTRUE(attr(query, "partial"))) {
+            match.str <- paste0("tokens.token LIKE :", nt) 
+        } else {
+            match.str <- paste0("tokens.token = :", nt) 
+        }
+
         field <- attr(query, "field")
         if (!is.null(field)) {
             nf <- add_query_parameter(env, field)
-            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid LEFT JOIN fields ON fields.fid = links.fid WHERE tokens.token = :%s AND fields.field = :%s)", name, nt, nf))
+            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid LEFT JOIN fields ON fields.fid = links.fid WHERE %s AND fields.field = :%s)", name, match.str, nf))
         } else {
-            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid WHERE tokens.token = :%s)", name, nt))
+            return(sprintf("%s IN (SELECT pid from links LEFT JOIN tokens ON tokens.tid = links.tid WHERE %s)", name, match.str))
         }
     }
 
@@ -183,12 +224,18 @@ build_query <- function(query, name, env) {
         for (i in seq_along(textual)) {
             current <- textual[[i]]
             nt <- add_query_parameter(env, current[[1]])
+            if (isTRUE(attr(current, "partial"))) {
+                match.str <- paste0("tokens.token LIKE :", nt) 
+            } else {
+                match.str <- paste0("tokens.token = :", nt) 
+            }
+
             field <- attr(current, "field")
             if (is.null(field)) {
-                textual[[i]] <- sprintf("tokens.token = :%s", nt)
+                textual[[i]] <- match.str
             } else {
                 nf <- add_query_parameter(env, field)
-                textual[[i]] <- sprintf("(tokens.token = :%s AND fields.field = :%s)", nt, nf)
+                textual[[i]] <- sprintf("(%s AND fields.field = :%s)", match.str, nf)
                 needs.field <- TRUE
             }
         }
