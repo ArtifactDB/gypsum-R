b92ced931eb74689f5343b40acaceb045a0d9f45
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-03-02 10:03:17 -0800

    Don't try anything with symlinks at all on windows.

--- tests/testthat/test-cloneVersion.R
+++ tests/testthat/test-cloneVersion.R
@@ -6,16 +6,22 @@ test_that("cloneVersion works as expected with existing files", {
     dest <- tempfile()
     out <- cloneVersion("test-R", "basic", "v1", destination=dest, cache=cache)
 
-    l1 <- Sys.readlink(file.path(dest, "blah.txt"))
-    expect_true(file.exists(l1))
+    d1 <- file.path(dest, "blah.txt")
     if (.Platform$OS.type != "windows") { # as windows might not support symbolic links.
+        l1 <- Sys.readlink(d1)
+        expect_true(file.exists(l1))
         expect_true(endsWith(l1, "test-R/basic/v1/blah.txt"))
+    } else {
+        expect_true(file.exists(d1))
     }
 
-    l2 <- Sys.readlink(file.path(dest, "foo/bar.txt"))
-    expect_true(file.exists(l2))
+    d2 <- file.path(dest, "foo/bar.txt")
     if (.Platform$OS.type != "windows") {
+        l2 <- Sys.readlink(d2)
+        expect_true(file.exists(l2))
         expect_true(endsWith(l2, "test-R/basic/v1/foo/bar.txt"))
+    } else {
+        expect_true(file.exists(d2))
     }
 })
 
